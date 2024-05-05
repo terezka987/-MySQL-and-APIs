55c2db906f601c1652c452b1774481b44a24b4f6
Tereza Gonsiorova <sgtgonsi@liverpool.ac.uk>
2024-05-05 20:59:33 +0200

    added comments

--- requirement.sql
+++ requirement.sql
@@ -6,6 +6,7 @@ SELECT * FROM roles;
 UPDATE courses SET isAvailable = 1 WHERE CourseID = 5;
 UPDATE courses SET isAvailable = 0 WHERE CourseID = 5;
 -- 2) Admins should be able to assign one or more courses to a teacher
+-- Find all users that are teachers
 SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Teacher";
 UPDATE courses SET TeacherID = 3 WHERE CourseID = 5;
 -- 3) Students can browse and list all the available courses and see the course title and course
@@ -13,6 +14,7 @@ UPDATE courses SET TeacherID = 3 WHERE CourseID = 5;
 SELECT * FROM courses LEFT JOIN users ON users.UserID = courses.TeacherID WHERE isAvailable = 1;
 -- 4) Students can enrol in a course. Students should not be able to enrol in a course more than
 -- once at each time.
+-- Find all users that are students
 SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Student";
 SELECT * FROM enrolments WHERE CourseID = 5 AND UserID = 10;
 INSERT INTO enrolments (CourseID, UserID) VALUES (5,10);
